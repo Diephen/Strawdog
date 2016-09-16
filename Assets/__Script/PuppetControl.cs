9ba2e87e1f495d69c810987a00784c65b50b6fd5
jsohn96 <jsohn96@hotmail.com>
2016-09-16 09:26:36 -0700

    Pickup Control Added

--- Assets/__Script/PuppetControl.cs
+++ Assets/__Script/PuppetControl.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class PuppetControl : MonoBehaviour {
 	[SerializeField] GameObject[] m_Finger = new GameObject[3];
-	[SerializeField] KeyCode[] m_ListenKey = new KeyCode[] {KeyCode.A, KeyCode.S, KeyCode.D};
+	[SerializeField] KeyCode[] m_ListenKey = new KeyCode[] {KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Space};
 	[SerializeField] float[] m_MoveDistance = new float[3];
 	[SerializeField] float[] m_CrouchMoveDistance = new float[3];
+	[SerializeField] float[] m_PickupMoveDistance = new float[3];
 	[SerializeField] bool[] m_Continuous = new bool[3];
 	bool[] m_Pressed = new bool[] { false, false, false };
 	Vector3[] oldPos = new Vector3[3];
 	Vector3[] newPos = new Vector3[3];
 	Vector3[] crouchPos = new Vector3[3];
+	Vector3[] PickupPos = new Vector3[3];
 
 	[SerializeField] float m_MoveSpeed = 2.0f;
 
-	enum charState {idle, left, right, crouch};
+	enum charState {idle, left, right, crouch, pickup};
 	charState m_charState = charState.idle;
 	float[] startTime = new float[3];
 	float[] distCovered = new float[3];
@@ -29,55 +31,71 @@ public class PuppetControl : MonoBehaviour {
 			//Define localPosition of travel for crouch Lerp
 			crouchPos [i] = oldPos [i];
 			crouchPos [i].y -= m_CrouchMoveDistance [i];
+			//Define localPosition of travel for Pickup Lerp
+			PickupPos [i] = oldPos [i];
+			PickupPos [i].y -= m_PickupMoveDistance [i];
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+	//Handle Pickup
+		if (Input.GetKeyDown (m_ListenKey [3])) {
+			m_charState = charState.pickup;
+			Pickup ();
+		} else if (Input.GetKeyUp (m_ListenKey [3])) {
+			m_charState = charState.idle;
+		}
+
+	//Handle Crouch
 		if(Input.GetKey (m_ListenKey[0]) && 
 		   Input.GetKey (m_ListenKey[1]) && 
-		   Input.GetKey (m_ListenKey[2])){
+		   Input.GetKey (m_ListenKey[2]) &&
+			m_charState !=  charState.pickup){
 			m_charState = charState.crouch;
 		}
 			
-		//A-Key
-		if (m_Continuous [0] && Input.GetKey (m_ListenKey [0]) 
-			&& (m_charState == charState.idle || m_charState == charState.left)) {
-			m_charState = charState.left;
-			Walk (m_charState);
-		}
-		if (Input.GetKeyDown (m_ListenKey [0])) {
-			m_Pressed[0] = true;
-			startTime[0] = Time.time;
-		} else if (Input.GetKeyUp (m_ListenKey [0])) {
-			m_Pressed[0] = false;
-			startTime[0] = Time.time;
-			m_charState = charState.idle;
-		}
+		if (m_charState != charState.pickup) {
+			//A-Key
+			if (m_Continuous [0] && Input.GetKey (m_ListenKey [0])
+			   && (m_charState == charState.idle || m_charState == charState.left)) {
+				m_charState = charState.left;
+				Walk (m_charState);
+			}
+			if (Input.GetKeyDown (m_ListenKey [0])) {
+				m_Pressed [0] = true;
+				startTime [0] = Time.time;
+			} else if (Input.GetKeyUp (m_ListenKey [0])) {
+				m_Pressed [0] = false;
+				startTime [0] = Time.time;
+				m_charState = charState.idle;
+			}
 
-		//S-Key
-		if (Input.GetKeyDown (m_ListenKey [1])) {
-			m_Pressed[1] = true;
-			startTime[1] = Time.time;
-		} else if (Input.GetKeyUp (m_ListenKey [1])) {
-			m_Pressed[1] = false;
-			startTime[1] = Time.time;
-			m_charState = charState.idle;
-		}
+			//S-Key
+			if (Input.GetKeyDown (m_ListenKey [1])) {
+				m_Pressed [1] = true;
+				startTime [1] = Time.time;
+			} else if (Input.GetKeyUp (m_ListenKey [1])) {
+				m_Pressed [1] = false;
+				startTime [1] = Time.time;
+				m_charState = charState.idle;
+			}
 
-		// D-Key
-		if (m_Continuous [2] && Input.GetKey (m_ListenKey [2])
-			&& (m_charState == charState.idle || m_charState == charState.right)) {
-			m_charState = charState.right;
-			Walk (m_charState);
-		}
-		if (Input.GetKeyDown (m_ListenKey [2])) {
-			m_Pressed[2] = true;
-			startTime [2] = Time.time;
-		} else if (Input.GetKeyUp (m_ListenKey [2])) {
-			m_Pressed[2] = false;
-			startTime[2] = Time.time;
-			m_charState = charState.idle;
+			// D-Key
+			if (m_Continuous [2] && Input.GetKey (m_ListenKey [2])
+			   && (m_charState == charState.idle || m_charState == charState.right)) {
+				m_charState = charState.right;
+				Walk (m_charState);
+			}
+			if (Input.GetKeyDown (m_ListenKey [2])) {
+				m_Pressed [2] = true;
+				startTime [2] = Time.time;
+			} else if (Input.GetKeyUp (m_ListenKey [2])) {
+				m_Pressed [2] = false;
+				startTime [2] = Time.time;
+				m_charState = charState.idle;
+			}
 		}
 
 		distCovered [0] = (Time.time - startTime [0]);
@@ -88,7 +106,10 @@ public class PuppetControl : MonoBehaviour {
 			m_Finger [0].transform.localPosition = Vector3.Lerp (m_Finger [0].transform.localPosition, crouchPos [0], distCovered [0]);
 			m_Finger [1].transform.localPosition = Vector3.Lerp (m_Finger [1].transform.localPosition, crouchPos [1], distCovered [1]);
 			m_Finger [2].transform.localPosition = Vector3.Lerp (m_Finger [2].transform.localPosition, crouchPos [2], distCovered [2]);
-
+		} else if (m_charState == charState.pickup) {
+			m_Finger [0].transform.localPosition = Vector3.Lerp (m_Finger [0].transform.localPosition, PickupPos [0], distCovered [0]);
+			m_Finger [1].transform.localPosition = Vector3.Lerp (m_Finger [1].transform.localPosition, PickupPos [1], distCovered [1]);
+			m_Finger [2].transform.localPosition = Vector3.Lerp (m_Finger [2].transform.localPosition, PickupPos [2], distCovered [2]);
 		} else {
 			if (m_Pressed [0]) {
 				m_Finger [0].transform.localPosition = Vector3.Lerp (m_Finger [0].transform.localPosition, newPos [0], distCovered [0]);
@@ -114,5 +135,9 @@ public class PuppetControl : MonoBehaviour {
 		} else if (dir == charState.right) {
 			transform.Translate (Vector2.right * m_MoveSpeed * Time.deltaTime);
 		}
+	}
+
+	void Pickup() {
+		
 	}
 }
