637721fc360ad85ff836032ad316fa73ae047b6e
jsohn96 <jsohn96@hotmail.com>
2016-09-19 11:28:35 -0700

    Background Scrolling Added

---
+++ Assets/__Script/BackgroundScroll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour
+{
+	public float tileSizeX;
+
+	Vector3 _bgPos;
+
+	void Start ()
+	{
+		_bgPos = transform.position;
+	}
+
+	void Update ()
+	{
+		if (Camera.main.transform.position.x > _bgPos.x + tileSizeX) {
+			_bgPos.x += tileSizeX * 2;
+			transform.position = _bgPos;
+		} else if (Camera.main.transform.position.x < _bgPos.x - tileSizeX) {
+			_bgPos.x -= tileSizeX * 2;
+			transform.position = _bgPos;
+		}
+	}
+}
