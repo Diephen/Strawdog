beaee55de6467e0588b02dd97afb6e837e2f6d5a
jsohn96 <jsohn96@hotmail.com>
2016-09-22 00:56:06 -0700

    Pick Up Further Implemented (Not Done)

--- Assets/__Script/Pickup.cs
+++ Assets/__Script/Pickup.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class Pickup : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
 	
@@ -10,20 +9,15 @@ public class Pickup : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	void PickupItem () {
 		
 	}
 
-	void OnTriggerStay2D(Collider2D other) {
-//		if (Input.GetKeyDown (KeyCode.Space) && other.CompareTag ("magicItems")) {
-//			_interactObj = other.GetComponent <Interact_Obj>();
-//			// add magic item to inventory
-//			_catInventory = _interactObj._magicItemsID;
-//			// call PickedUp function in Interact_Obg
-//			_interactObj.PickedUp (gameObject.transform);
-//		}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "Bucket") {
+			other.transform.SetParent (gameObject.transform);
+		}
 	}
 }
