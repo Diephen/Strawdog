9e5d99b29ae0c3c176a8682fd74d515690e58409
jsohn96 <jsohn96@hotmail.com>
2016-10-09 00:52:19 -0700

    FlashLight Rotation Implemented

--- Assets/rotateTowards.cs
+++ Assets/rotateTowards.cs
@@ -4,29 +4,28 @@ using System.Collections;
 public class rotateTowards : MonoBehaviour {
 	[SerializeField] Transform target;
 	[SerializeField] float speed;
+	[SerializeField] AnimationCurve _spotAngleCurve;
 	Quaternion _rotation;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-//		Vector3 targetDir = target.position - transform.position;
-//		float step = speed * Time.deltaTime;
-//		Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
-//		Debug.DrawRay(transform.position, newDir, Color.red);
-//		transform.rotation = Quaternion.LookRotation(newDir);
-//
+	Light _spotLight;
 
-		Vector2 dir = target.position;
+	void Start() {
+		_spotLight = gameObject.GetComponent <Light> ();
+	}
+
+	void Update () {
+		Vector2 dir = target.position - transform.position;
 
 		float angle = Vector2.Angle(Vector2.right, dir);
 
-		angle = dir.y < 0 ? -angle : angle;
+
+		angle = dir.y < 0 ? angle : -angle;
 
 		_rotation = Quaternion.Euler (angle, 90f, transform.rotation.z);
 		transform.localRotation = _rotation;
-//		transform.Rotate(Vector3.forward, speed * Time.deltaTime * angle);
+		_spotLight.spotAngle = MathHelpers.LinMapFrom01 (40.0f, 150.0f, _spotAngleCurve.Evaluate (MathHelpers.LinMapTo01 (0.0f, 180.0f, angle)));
+
+
+
+
 	}
 }
