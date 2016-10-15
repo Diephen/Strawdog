15b0fa46a71ed44ed1e05ccc7643fd8c41dba5bb
jsohn96 <jsohn96@hotmail.com>
2016-10-15 14:40:03 -0700

    Checkpoint implemented

--- Assets/TowerPatrol.cs
+++ Assets/TowerPatrol.cs
@@ -11,6 +11,7 @@ public class TowerPatrol : MonoBehaviour {
 
 	Timer _towerTimer;
 	Timer _towerCooldownTimer;
+	Timer _caughtTimer;
 
 	bool _lookAtFollow = false;
 	GameObject _followPerson;
@@ -18,10 +19,17 @@ public class TowerPatrol : MonoBehaviour {
 	float _currentRotationZ;
 	bool _reRotate = false;
 
+	SpriteRenderer _towerLightSprite;
+	Color _currentColor;
+
+
 	// Use this for initialization
 	void Start () {
+		_caughtTimer = new Timer (5.0f);
 		_towerTimer = new Timer (5.0f);
 		_towerCooldownTimer = new Timer (2.0f);
+		_towerLightSprite = transform.GetChild (0).GetComponent<SpriteRenderer>();
+		_currentColor = _towerLightSprite.color;
 	}
 	
 	// Update is called once per frame
@@ -55,8 +63,13 @@ public class TowerPatrol : MonoBehaviour {
 			crossProduce = Vector3.Cross (Vector3.down, dir);
 			angle = crossProduce.z > 0 ? angle : -angle;
 			transform.localRotation = Quaternion.Euler (0f, 0f, angle);
+			_towerLightSprite.color = Color.Lerp (_currentColor, Color.red, _caughtTimer.PercentTimePassed);
+			if (_caughtTimer.IsOffCooldown) {
+				Events.G.Raise (new CaughtSneakingEvent ());
+			}
 		}
 		else if (_reRotate) {
+			_towerLightSprite.color = Color.Lerp (_currentColor, Color.white, _towerCooldownTimer.PercentTimePassed);
 			if (_isLeft) {
 				//50 to -50 -->
 				angle = MathHelpers.LinMapFrom01 (_currentRotationZ, _towerPatrolRange.Max, _towerCooldownTimer.PercentTimePassed);
@@ -77,6 +90,8 @@ public class TowerPatrol : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.name == "FemaleStructure") {
 			_lookAtFollow = true;
+			_currentColor = _towerLightSprite.color;
+			_caughtTimer.Reset ();
 			_followPerson = other.gameObject;
 		}
 	}
@@ -92,6 +107,7 @@ public class TowerPatrol : MonoBehaviour {
 		if (_currentRotationZ > 100.0f) {
 			_currentRotationZ = _currentRotationZ - 360.0f;
 		}
+		_currentColor = _towerLightSprite.color;
 		_reRotate = true;
 		_lookAtFollow = false;
 		_towerCooldownTimer.Reset ();
---
+++ Assets/__Script/Act2/Checkpoint.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+	[SerializeField] float _checkPosition = 0.0f;
+	Fading _fadingScript;
+	// Use this for initialization
+	void Start () {
+		_fadingScript = GameObject.Find ("Fade").GetComponent<Fading> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Checkpoint") {
+			_checkPosition = other.transform.position.x;
+		}
+	}
+
+	void Caught(CaughtSneakingEvent e){
+		StartCoroutine (Respawn ());
+	}
+
+	IEnumerator Respawn(){
+		yield return new WaitForSeconds(2.0f);
+		float fadeTime = _fadingScript.BeginFade (1);
+		yield return new WaitForSeconds(fadeTime);
+		Vector3 _tempPos = transform.position;
+		_tempPos.x = _checkPosition;
+		transform.position = _tempPos;
+		yield return new WaitForSeconds(1.0f);
+		_fadingScript.BeginFade (-1);
+	}
+
+	void OnEnable(){
+		Events.G.AddListener<CaughtSneakingEvent>(Caught);
+	}
+
+	void OnDisable(){
+		Events.G.RemoveListener<CaughtSneakingEvent>(Caught);
+	}
+}
