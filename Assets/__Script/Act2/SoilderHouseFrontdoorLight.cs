ad6ab62e38934ce43a1fdd0ed7dd4947e433082e
YIWEN DAI <daiyw1992@gmail.com>
2016-10-19 15:18:49 -0700

    soldier house done

---
+++ Assets/__Script/Act2/SoilderHouseFrontdoorLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoilderHouseFrontdoorLight : MonoBehaviour {
+	Animator m_anim;
+
+	// Use this for initialization
+	void Start () {
+		m_anim = GetComponent<Animator> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.name == "FemaleStructure") {
+			m_anim.SetTrigger ("TriggerFlicker");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.name == "FemaleStructure") {
+			m_anim.SetTrigger ("TriggerFlicker");
+		}
+	}
+}
