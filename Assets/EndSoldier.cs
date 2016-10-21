052f12bef6efda5e8aae736caa4e43178d602f37
YIWEN DAI <daiyw1992@gmail.com>
2016-10-21 01:43:13 -0700

    Add tutorial left/right ends

---
+++ Assets/EndSoldier.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndSoldier : MonoBehaviour {
+	[SerializeField] Animator m_anim;
+	// Use this for initialization
+	void Awake () {
+		if (GetComponent<Animator> () != null) {
+			m_anim = GetComponent<Animator> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		//Debug.Log ("Attention");
+		if(other.name == "GuardStructure"){
+			m_anim.SetBool ("IsNofuther", true);
+			Debug.Log ("Attention");
+		}
+
+	}
+
+
+	void OnTriggerExit2D(Collider2D other) {
+		//Debug.Log ("Guard Pass");
+		if(other.name == "GuardStructure"){
+			m_anim.SetBool ("IsNofuther", false);
+			Debug.Log ("Attention");
+		}
+
+	}
+}
