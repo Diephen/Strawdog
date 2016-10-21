220b458b19125c70cbbaf1a19528bce97ce45cb7
jsohn96 <jsohn96@hotmail.com>
2016-10-20 23:48:02 -0700

    Audio Controller Changes Started

--- Assets/AudioController.cs
+++ Assets/AudioController.cs
@@ -2,16 +2,14 @@
 using System.Collections;
 
 public class AudioController : MonoBehaviour {
-	AudioSource _audioSource;
 	[SerializeField] float[] volume = new float[] {0.1f, 0.3f, 0.7f};
-
+	[SerializeField] AudioSource _audioSource;
+	[SerializeField] AudioSource _soundSource1;
 	AudioSource _tempAudioSource;
 	float _goalVolume;
 
 	void Start () {
-		_audioSource = gameObject.GetComponent <AudioSource> ();
 		_audioSource.volume = volume [0];
-
 	}
 	
 	// Update is called once per frame
@@ -33,12 +31,28 @@ public class AudioController : MonoBehaviour {
 	{
 		Events.G.AddListener<GuardEnteringCellEvent>(OnGuardEnterCell);
 		Events.G.AddListener<GuardEngaginPrisonerEvent>(OnGuardEngagePrisoner);
+
+		//Act2
+		Events.G.AddListener<LockCellEvent>(OnGuardEngagePrisoner);
+		Events.G.AddListener<PrisonerFoundBombEvent>(OnGuardEngagePrisoner);
+		Events.G.AddListener<GuardFoundBombEvent>(OnGuardEngagePrisoner);
+		Events.G.AddListener<Act2_GuardWalkedUpStairsEvent>(OnGuardEngagePrisoner);
+		Events.G.AddListener<Act2_PrisonerWalkedUpStairsEvent>(OnGuardEngagePrisoner);
+		Events.G.AddListener<Act2_PrisonerWalkedDownStairsEvent>(OnGuardEngagePrisoner);
 	}
 
 	void OnDisable ()
 	{
 		Events.G.RemoveListener<GuardEnteringCellEvent>(OnGuardEnterCell);
 		Events.G.RemoveListener<GuardEngaginPrisonerEvent>(OnGuardEngagePrisoner);
+
+		//Act2
+		Events.G.RemoveListener<LockCellEvent>(OnGuardEngagePrisoner);
+		Events.G.RemoveListener<PrisonerFoundBombEvent>(OnGuardEngagePrisoner);
+		Events.G.RemoveListener<GuardFoundBombEvent>(OnGuardEngagePrisoner);
+		Events.G.RemoveListener<Act2_GuardWalkedUpStairsEvent>(OnGuardEngagePrisoner);
+		Events.G.RemoveListener<Act2_PrisonerWalkedUpStairsEvent>(OnGuardEngagePrisoner);
+		Events.G.RemoveListener<Act2_PrisonerWalkedDownStairsEvent>(OnGuardEngagePrisoner);
 	}
 
 	void OnGuardEnterCell (GuardEnteringCellEvent e)
