ad8f07270082641e13fa4863595be6e17c6ec43d
jsohn96 <jsohn96@hotmail.com>
2016-10-20 21:28:23 -0700

    UI Toggle

---
+++ Assets/HelpMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class HelpMenu : MonoBehaviour {
+	Canvas _canvas;
+	// Use this for initialization
+	void Start () {
+		_canvas = gameObject.GetComponent<Canvas> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.H)) {
+			_canvas.enabled = !_canvas.enabled;
+		}
+	}
+}
