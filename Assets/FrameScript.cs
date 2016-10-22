7b010e1683ea43443aa8136ca72d210e975c6517
jsohn96 <jsohn96@hotmail.com>
2016-10-22 16:12:31 -0700

    Frame SOund added

--- Assets/FrameScript.cs
+++ Assets/FrameScript.cs
@@ -13,15 +13,19 @@ public class FrameScript : MonoBehaviour {
 	Timer _flapTimer = new Timer(1.0f);
 	bool _open = false;
 
+	[SerializeField] AudioClip _openSound;
+	[SerializeField] AudioClip _closeSound;
+	AudioSource _audioSource;
+
 	// Use this for initialization
 	void Start () {
 		_leftTempPos = _leftFlap.transform.localPosition;
 		_rightTempPos = _rightFlap.transform.localPosition;
+		_audioSource = gameObject.GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if (!_flapTimer.IsOffCooldown) {
 			if (_open) {
 				_leftTempPos.x = MathHelpers.LinMapFrom01 (_leftFlapRange.Min, _leftFlapRange.Max, _flapTimer.PercentTimeLeft);
 				_rightTempPos.x = MathHelpers.LinMapFrom01 (_rightFlapRange.Min, _rightFlapRange.Max, _flapTimer.PercentTimePassed);
@@ -32,25 +36,33 @@ public class FrameScript : MonoBehaviour {
 			}
 		_leftFlap.transform.localPosition = _leftTempPos;
 		_rightFlap.transform.localPosition = _rightTempPos;
-//		}
 
 		if (Input.GetKeyDown (KeyCode.Q)) {
 			_flapTimer.Reset ();
 			_open = true;
+			_audioSource.clip = _openSound;
+			_audioSource.Play ();
 		}
 		if (Input.GetKeyDown (KeyCode.W)) {
 			_flapTimer.Reset ();
 			_open = false;
+			_audioSource.clip = _closeSound;
+			_audioSource.Play ();
 		}
 	}
 
 	public void CloseFlap(){
 		_flapTimer.Reset();
 		_open = false;
+		_audioSource.clip = _closeSound;
+		_audioSource.Play ();
+
 	}
 
 	public void OpenFlap(){
 		_flapTimer.Reset ();
 		_open = true;
+		_audioSource.clip = _openSound;
+		_audioSource.Play ();
 	}
 }
