1a80b1c6e89cd857e1b3091ef6794888648ca610
YIWEN DAI <daiyw1992@gmail.com>
2016-10-29 21:08:53 -0700

    after shot turn black

--- Assets/ShotDeathPrisonerHandle.cs
+++ Assets/ShotDeathPrisonerHandle.cs
@@ -5,6 +5,11 @@ public class ShotDeathPrisonerHandle : MonoBehaviour {
 	
 	[SerializeField] Animator m_Anim;
 	[SerializeField] BoxCollider2D m_StopCol;
+	[SerializeField] SpriteRenderer[] m_Sprites;
+	[SerializeField] Color m_EndColor;
+	[SerializeField] Color m_StartColor;
+	[SerializeField] float m_Duration;
+	Timer m_ColorTimer;
 	BoxCollider2D m_TriggerCol;
 	bool isExecuted = false;
 
@@ -12,11 +17,19 @@ public class ShotDeathPrisonerHandle : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		m_TriggerCol = gameObject.GetComponent<BoxCollider2D> ();
+		m_Sprites = gameObject.GetComponentsInChildren<SpriteRenderer> ();
+		foreach (SpriteRenderer spr in m_Sprites) {
+			spr.material.color = m_StartColor;
+		}
+		m_ColorTimer = new Timer (m_Duration);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isExecuted) {
+			TurnDark ();
+		}
 	
 	}
 
@@ -26,6 +39,14 @@ public class ShotDeathPrisonerHandle : MonoBehaviour {
 			m_Anim.Play ("SP-death");
 			m_StopCol.enabled = false;
 			m_TriggerCol.enabled = false;
+			m_ColorTimer.Reset ();
 		}
+	}
+
+	void TurnDark(){
+		foreach (SpriteRenderer spr in m_Sprites) {
+			spr.material.color = Color.Lerp (m_StartColor, m_EndColor, m_ColorTimer.PercentTimePassed);
+		}
+		//lerpedColor = Color.Lerp(Color.white, Color.black, Time.deltaTime);
 	}
 }
