62c52ed07536c9bbfc6170e6957f159c45d558d5
YIWEN DAI <daiyw1992@gmail.com>
2016-11-02 12:25:03 -0700

    Scene fix

--- Assets/TutorialTree.cs
+++ Assets/TutorialTree.cs
@@ -6,6 +6,7 @@ public class TutorialTree : MonoBehaviour {
 	[SerializeField] Animator m_Anim;
 	[SerializeField] GameObject m_DropFlower;
 	int m_BumpCnt = 0;
+	bool m_IsFlowerGone = false;
 	// Use this for initialization
 	void Start () {
 		if (GetComponent<Animator> ()) {
@@ -21,8 +22,14 @@ public class TutorialTree : MonoBehaviour {
 			m_DropFlower.GetComponent<HingeJoint2D> ().enabled = false;
 		}
 
-		if (m_DropFlower.transform.position.y <= -15) {
-			Destroy(m_DropFlower);
+		if (m_DropFlower != null && !m_IsFlowerGone) {
+			if( m_DropFlower.transform.position.y <= -15){
+				//Destroy(m_DropFlower);
+				m_DropFlower.GetComponent<BoxCollider2D>().enabled = false;
+				m_DropFlower.GetComponent<Rigidbody2D> ().isKinematic = true;
+				m_IsFlowerGone = true;
+			}
+
 		}
 	}
 
