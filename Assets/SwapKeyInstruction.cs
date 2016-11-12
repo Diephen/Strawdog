ef0e73e36e1b47a83f8e4c55226e5b0c6032599c
jsohn96 <jsohn96@hotmail.com>
2016-11-11 16:10:48 -0800

    Glow Works

--- Assets/SwapKeyInstruction.cs
+++ Assets/SwapKeyInstruction.cs
@@ -5,7 +5,9 @@ public class SwapKeyInstruction : MonoBehaviour {
 	[SerializeField] Sprite _nonPress;
 	[SerializeField] Sprite _Press;
 	[SerializeField] KeyCode _key;
+	[SerializeField] Sprite _glow;
 	SpriteRenderer _sp;
+	bool _isGlow = false;
 
 	void Start(){
 		_sp = gameObject.GetComponent<SpriteRenderer> ();
@@ -13,11 +15,45 @@ public class SwapKeyInstruction : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (_key)) {
-			_sp.sprite = _Press;
+		if (_isGlow) {
+			if (Input.GetKey (_key)) {
+				_sp.sprite = _Press;
+			} 
+			else if (Input.GetKeyUp (_key)) {
+				_sp.sprite = _nonPress;
+			}
+			else if (_glow != null) {
+				_sp.sprite = _glow;
+			}
 		}
-		if (Input.GetKeyUp (_key)) {
-			_sp.sprite = _nonPress;
+		else {
+			if (Input.GetKey (_key)) {
+				_sp.sprite = _Press;
+			}
+			else if (Input.GetKeyUp (_key)) {
+				_sp.sprite = _nonPress;
+			}
+			else {
+				_sp.sprite = _nonPress;
+			}
 		}
+
+	}
+
+	void EncounterTouch(EncounterTouchEvent e){
+		if (e.OnGuard) {
+			_isGlow = true;
+		}
+		else {
+			_isGlow = false;
+		}
+	}
+
+	void OnEnable(){
+		Events.G.AddListener<EncounterTouchEvent>(EncounterTouch);
+	}
+
+	void OnDisable(){
+		Events.G.RemoveListener<EncounterTouchEvent>(EncounterTouch);
 	}
 }
