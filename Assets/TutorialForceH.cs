620d6f87b8e6213f214cb3c694a2401d036706fa
jsohn96 <jsohn96@hotmail.com>
2016-11-21 01:29:02 -0800

    Tutorial Adjustments

---
+++ Assets/TutorialForceH.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialForceH : MonoBehaviour {
+	[SerializeField] MeshRenderer _meshRenderer;
+	[SerializeField] TextMesh _textmesh;
+	Timer _nextTimer = new Timer(1.0f);
+	MeshRenderer _thisMesh;
+	bool _once = false;
+
+	Color _tempTextColor;
+
+	void Start(){
+		Events.G.Raise (new DisableMoveEvent ());
+		_tempTextColor = _textmesh.color;
+		_thisMesh = GetComponent<MeshRenderer> ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.H) && !_once) {
+			Events.G.Raise (new EnableMoveEvent ());
+			_nextTimer.Reset ();
+			_meshRenderer.enabled = false;
+			_thisMesh.enabled = false;
+			_once = true;
+		}
+
+		if (_once) {
+			float tempAlpha = Mathf.Lerp (0.0f, 1.0f, _nextTimer.PercentTimePassed);
+			_tempTextColor.a = tempAlpha;
+			_textmesh.color = _tempTextColor;
+		}
+
+	}
+}
