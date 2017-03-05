e1ecd4898a315c6f1a9ed4c27eadc9044d2440cb
Jung-Ho Sohn <junghos@usc.edu>
2017-03-05 09:01:42 -0800

    tutorial modified

---
+++ Assets/FillInTutorialText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillInTutorialText : MonoBehaviour {
+
+	TextMesh _textMesh;
+
+	// Use this for initialization
+	void Start () {
+		_textMesh = gameObject.GetComponent<TextMesh>();
+		string text = "Hold the <i>'"+GameStateManager.gameStateManager._pDownKey.ToString()+"'</i> key and the <i>'"+GameStateManager.gameStateManager._gDownKey.ToString()+"'</i> \nwhen the characters glow to begin";
+		_textMesh.text = text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
