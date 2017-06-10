2883b0e2ab5126027ada69b2f4d4a919129aa73e
Jung-Ho Sohn <junghos@usc.edu>
2017-06-10 16:08:00 -0700

    Minor

--- Assets/TriggerWarning.cs
+++ Assets/TriggerWarning.cs
@@ -12,7 +12,7 @@ public class TriggerWarning : MonoBehaviour {
 	
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Space)){
-			Events.G.Raise (new LoadMainMenuEvent ());
+			Events.G.Raise (new LoadTutorialEvent ());
 			_text.color = Color.white;
 		}
 	}
